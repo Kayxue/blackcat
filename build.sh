768f67e937838be1021bd6e60f8d2163532f2017
wolf-yuan-6115 <58245644+wolf-yuan-6115@users.noreply.github.com>
2021-10-09 00:13:53 +0000

    Deploying to gh-pages from @ blackcatbot/blackcat@d0bb694784f7079f75c4ad27e9f769f83086eca2 🚀

--- build.sh
+++
@@ -1,7 +0,0 @@
-npm i -g purgecss
-
-cp ./src ./dist -r
-cd dist
-mkdir css
-curl -o css/bootstrap.css https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/css/bootstrap.min.css
-purgecss --css css/bootstrap.css --content index.html --output css/bootstrap.min.css
