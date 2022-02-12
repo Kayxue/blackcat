936f4166ec4a719e00a54d47aca57e1a7af556b7
wolf-yuan-6115 <h1030035@gmail.com>
2022-02-12 23:29:32 +0800

    Add volume notify command

---
+++ src/commands/volume.js
@@ -0,0 +1,20 @@
+import { MessageEmbed } from "discord.js";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { danger } from "../color.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("volume")
+    .setDescription("變更音量"),
+  run: function (interaction) {
+    let volumeEmbed = new MessageEmbed()
+      .setTitle("🙁 音量調整已被移除") 
+      .setDescription(
+        "為了提供更好的音質，音量調整已被移除\n"+
+        "如果您仍要調整音量，請在我的頭貼上點選右鍵來調整音量")
+      .setColor(danger);
+    interaction.reply({
+      embeds: [volumeEmbed]
+    }).catch(() => {});
+  }
+};
