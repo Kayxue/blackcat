57ec4d4deb2635dd1bb4e18a35ec5031de01a46b
Wolf yuan <h1030035@gmail.com>
2022-02-13 20:20:04 +0800

    Update slash command register

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,3 @@
-import slash from "../util/slashRegister.js";
 import log from "../logger.js";
 
 export default {
@@ -6,7 +5,15 @@ export default {
   once: false,
   run: async (client) => {
     log.info(`${client.user.username} 已上線`);
-    const commands = client.commands.map((e) => e.data.toJSON());
-    slash(client, commands);
+    let commands = client.commands.map((e) => e.data.toJSON());
+    let registered;
+    log.info("正在同步指令...");
+    try {
+      registered = await client.application?.commands.set(commands);
+    } catch (e) {
+      log.error(`同步指令時發生錯誤: ${e.message}`, e);
+      return;
+    }
+    log.info(`同步指令成功: ${registered.size}`);
   },
 };
