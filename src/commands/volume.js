d6a9f54e17e2500a7d06b566c53cf79a08fbdaf3
Wolf yuan <h1030035@gmail.com>
2022-03-05 00:04:03 +0800

    Add volume

--- src/commands/volume.js
+++ src/commands/volume.js
@@ -1,5 +1,10 @@
 import { MessageEmbed } from "discord.js";
-import { danger } from "../color.js";
+import PlayerManager from "../audio/PlayerManager.js";
+import allowModify from "../util/allowModify.js";
+import {
+  danger,
+  success
+} from "../color.js";
 
 export default {
   data: {
@@ -8,17 +13,32 @@ export default {
     options: [
       {
         name: "volume",
-        description: "音量大小",
+        description: "音量大小，留空會顯示目前的音量",
         type: 4,
-        required: true
+        required: false
       }
     ]
   },
   run: function (interaction) {
-    const embed = new MessageEmbed()
-      .setColor(danger)
-      .setTitle("音量調整")
-      .setDescription(`音量調整為 ${interaction.options.volume}`);
-    interaction.message.channel.send(embed);
+    let player;
+    if (!PlayerManager.getSendingPlayer(interaction.client, interaction.guild.id)) {
+      return interaction.reply("❌ 必須要有音樂正在播放");
+    } else {
+      player = PlayerManager.getSendingPlayer(interaction.client, interaction.guild.id);
+      if (!allowModify(interaction))
+        return interaction.reply("❌ 你必須加入一個語音頻道");
+    }
+    if (interaction.options.getInteger("volume")) {
+      if (interaction.options.getInteger("volume") > 200) {
+        return interaction.reply("❌ 音量不能大於 200");
+      } else if (interaction.options.getInteger("volume") < 0) {
+        return interaction.reply("❌ 音量不能小於 0");
+      }
+
+      player.volume = interaction.options.getInteger("volume") / 100;
+      interaction.reply("🔊 音量設定為：" + player.volume * 100 + "%");
+    } else {
+      interaction.reply("🔊 目前音量：" + player.volume * 100 + "%");
+    }
   }
 };
