ca78f0d4f1d77c2990baeeb70433959e3aba42fb
wolf-yuan-6115 <h1030035@gmail.com>
2022-03-17 17:12:20 +0800

    fix(event): Bot crashing after left guild

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -3,9 +3,9 @@ import PlayerManager from "../audio/PlayerManager.js";
 export default {
   event: "guildDelete",
   once: false,
-  run: (guild) => {
-    if (PlayerManager.getSendingPlayer(guild.client, guild.id)) {
-      let player = PlayerManager.getSendingPlayer(guild.client, guild.id);
+  run: (client, guild) => {
+    if (PlayerManager.getSendingPlayer(client, guild.id)) {
+      let player = PlayerManager.getSendingPlayer(client, guild.id);
       player.stop(null, true);
     }
   },
