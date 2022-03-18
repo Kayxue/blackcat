bc1d6cddce4258b2b491066911db2875c6483e87
Wolf yuan <h1030035@gmail.com>
2022-03-18 23:26:16 +0800

    fix(bot): Fix incorrect status

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,22 +5,11 @@ export default {
   once: false,
   run: async (client) => {
     log.info(`${client.user.username} 已上線`);
-    let commands = client.commands.map((e) => e.data);
-    let registered;
-    log.info("正在同步指令...");
-    try {
-      if (client.config.enableDev) log.info("開發者模式已開啟");
-      registered = await client.application?.commands.set(commands, client.config.enableDev ? client.config.slashGuild : undefined);
-    } catch (e) {
-      log.error(`同步指令時發生錯誤: ${e.message}`, e);
-      return;
-    }
-    log.info(`同步指令成功: ${registered.size}`);
     
     client.user.setPresence({
       status: "dnd",
       activities: [{
-        name: "/help | 黑貓測試版",
+        name: "/help | catmusic.ml",
         type: "LISTENING"
       }]
     });
