56fe41496f62fdbb057b4b0cf8c20ce280fb131a
wolf-yuan-6115 <h1030035@gmail.com>
2022-03-21 06:48:15 +0000

    Add join voice channel embed

---
+++ src/util/joinVC.js
@@ -0,0 +1,12 @@
+import { MessageEmbed } from "discord.js";
+import { danger } from "../color";
+
+export default function joinVC(interaction) {
+  let vcEmbed = new MessageEmbed()
+    .setTitle("❌ 你必須跟我在同一個頻道裡!")
+    .setDescription(`你必須在 <#${interaction.guild.me.voice.channel?.id}> 才可以使用 **/${interaction.commandName}**`)
+    .setColor(danger);
+  return interaction.reply({
+    embeds: [vcEmbed]
+  }).catch(() => {});
+}
