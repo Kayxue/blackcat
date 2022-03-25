79f7e6b15568643397836062e37f7eba74cc00d2
Wolf yuan <h1030035@gmail.com>
2022-03-25 22:16:48 +0800

    Update logger

--- src/logger.js
+++ src/logger.js
@@ -1,23 +1,21 @@
 import chalk from "chalk";
-
-let now = new Date();
-let time = chalk.dim(`${now.getMonth() + 1}/${now.getDate()} `) + chalk.cyan(`${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`);
+import getDateString from "./util/getDateString.js";
 
 function info(msg) {
-  console.log(time + " " + chalk.blue("資訊") + " " + msg);
+  console.log(getDateString() + " " + chalk.blue("資訊") + " " + msg);
 }
 
 function warn(msg) {
-  console.log(time + " " + chalk.yellowBright("警告") + " " + msg);
+  console.log(getDateString() + " " + chalk.yellowBright("警告") + " " + msg);
 }
 
 function error(msg, error) {
-  console.error(time + " " + chalk.red("錯誤") + " " + msg);
+  console.error(getDateString() + " " + chalk.red("錯誤") + " " + msg);
   if (error) {
     let spilted = error.stack.split("\n"),
       prefixed = [];
     for (let line of spilted) {
-      prefixed.push(time + " " + chalk.yellow("偵錯") + " " + line);
+      prefixed.push(getDateString() + " " + chalk.yellow("偵錯") + " " + line);
     }
     console.error(prefixed.join("\n"));
   }
---
+++ src/util/getDateString.js
@@ -0,0 +1,11 @@
+import chalk from "chalk";
+
+export default function getDateString() {
+  let month = (new Date().getMonth() + 1).toString().length === 1 ? `0${new Date().getMonth() + 1}` : new Date().getMonth() + 1;
+  let date = new Date().getDate().toString().length === 1 ? `0${new Date().getDate()}` : new Date().getDate();
+  let hours = new Date().getHours().toString().length === 1 ? `0${new Date().getHours()}` : new Date().getHours();
+  let minutes = new Date().getMinutes().toString().length === 1 ? `0${new Date().getMinutes()}` : new Date().getMinutes();
+  let seconds = new Date().getSeconds().toString().length === 1 ? `0${new Date().getSeconds()}` : new Date().getSeconds();
+  
+  return chalk.dim(`${month}/${date}`) + " " + chalk.cyan(`${hours}:${minutes}:${seconds}`);
+}
