8a321a201fe15e0affb244719294134d203271ea
wolf-yuan-6115 <h1030035@gmail.com>
2022-03-30 02:41:50 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
-if $1 == "--gitpod"
+if [ "$1" == "--gitpod" ]
 then
-  sudo apt-get install cmake
-  sudo apt-get remove ninja-build
+  sudo apt-get install -qqy cmake
+  sudo apt-get remove -qqy ninja-build
 else
-  if ! command -v cmake &> /dev/null
+  if [ ! command -v cmake &> /dev/null ]
   then
       echo "cmake could not be found"
-      if $1 == "--install"
+      if "$1" == "--install"
       then
           echo "Installing cmake"
-          sudo apt-get install cmake
+          sudo apt-get -qqy install cmake
       else
           exit 1
       fi
