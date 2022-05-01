3cf5429f6d9ea75d38c415ee67b8d79a592df299
wolf-yuan-6115 <h1030035@gmail.com>
2022-05-01 11:54:18 +0800

    Add requirePlayer function

---
+++ src/util/requirePlayer.js
@@ -0,0 +1,23 @@
+import PlayerManager from "../audio/PlayerManager.js";
+import allowModify from "../util/allowModify.js";
+import joinVC from "../util/joinVC.js";
+
+export default function requirePlayer(interaction, callback) {
+  let player;
+  if (
+    !PlayerManager.getSendingPlayer(
+      interaction.client,
+      interaction.guild.id,
+    )
+  ) {
+    return interaction.reply("❌ 必須要有音樂正在播放");
+  } else {
+    player = PlayerManager.getSendingPlayer(
+      interaction.client,
+      interaction.guild.id,
+    );
+    if (!allowModify(interaction)) return joinVC(interaction);
+  }
+  
+  callback(player);
+}
