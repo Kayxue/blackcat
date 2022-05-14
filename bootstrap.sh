03a3821cf5ad3e4456d17d433c7ae99ef582697c
wolf-yuan-6115 <h1030035@gmail.com>
2022-05-14 14:53:00 +0000

    Add bootstrap file to remove and install deps in gitpod

---
+++ bootstrap.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt remove ninja-build
+sudo apt install fonts-noto-unhinted fonts-noto-mono fonts-noto-hinted fonts-noto-extra fonts-noto-core fonts-noto-color-emoji fonts-noto-cjk-extra fonts-noto-cjk fonts-notofonts-noto-hint
