5a8f978dbd6d2c0bbc8c1c1b2a4aaa72e746f700
wolf-yuan-6115 <h1030035@gmail.com>
2022-07-26 16:59:45 +0800

    Fix error

--- src/util/allowModify.js
+++ src/util/allowModify.js
@@ -1,6 +1,6 @@
 export default (interaction) => {
   return (
     interaction.member.voice?.channel?.id ===
-    interaction.member.guild?.me.voice?.channel?.id
+    interaction.member.guild?.members.me.voice?.channel?.id
   );
 };
