a4935ab54a9700f32c1255fddff0bc390070686e
wolf-yuan-6115 <h1030035@gmail.com>
2023-05-22 13:36:38 +0800

    Fixes config error

--- src/util/configReslover.js
+++ src/util/configReslover.js
@@ -21,15 +21,9 @@ export default async function () {
       log.error("`token`無效", undefined, "設定");
       invaild = true;
     }
-    if (
-      config.debug.toString().toLowerCase() !== "true" &&
-      config.debug.toString().toLowerCase() !== "false"
-    ) {
-      log.error("`debug`不是一個布林值", undefined, "設定");
-      invalid = true;
-    } else {
-      config.debug = config.debug.toString().toLowerCase() === "true";
-    }
+
+    if (config.debug?.toString().toLowerCase() === "true")
+      config.debug = true;
 
     if (invaild) {
       log.error("設定出現錯誤，程式正在關閉", undefined, "設定");
